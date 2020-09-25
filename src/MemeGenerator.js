4568cf21a4ef3ef8e924b9bf3e75efa4a475b0b3
corbu0130 <johncorbeta1997@gmail.com>
2020-09-25 22:58:29 +0800

    Turned back initial image and response input.

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -7,7 +7,7 @@ class MemeGenerator extends React.Component {
     state = {
         topText: "",
         bottomText: "",
-        randomImage: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
         allMemeImgs: []
     }
 
@@ -28,26 +28,27 @@ class MemeGenerator extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        if (this.state.topText.length>0 && this.state.bottomText.length>0) {
+        // if (this.state.topText.length>0 && this.state.bottomText.length>0) {
             const ind = Math.floor(Math.random()*100)
             const imgurl = this.state.allMemeImgs[ind].url
             this.setState({randomImage: imgurl})
-        }
+        // }
     }
 
     render() {
-        const submit = this.state.randomImage.length>0
+        // const submit = this.state.randomImage.length>0
         return (
             <div>
                 <MemeForm key="meme-form"
                     handleChange={this.handleChange}
                     handleSubmit={this.handleSubmit}
                     item={this.state} />
-                {
+                {/* {
                     submit ?
                     <Meme key="meme" item={this.state} /> :
                     null
-                }
+                } */}
+                <Meme key="meme" item={this.state} />
             </div>
         )
     }
